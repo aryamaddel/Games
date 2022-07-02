de6464265f620b84620100aeb747f54596970891
Arya Maddel <84805906+aryamaddel@users.noreply.github.com>
2022-07-02 08:58:10 +0530

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -8,14 +8,29 @@ def play():
     print("Computer thinking..")
     sleep(1)
     print("Computer thinking...")
-    sleep(1)
-    hand = random.choice(['r', 'p', 's'])
-    guess = input(
+    computer_hand = random.choice(['r', 'p', 's'])
+    user_hand = input(
         "\nEnter your guess rock - 'r' , paper - 'p' , scissors - 's'\n: ")
-    if guess == hand:
-        print("\nYou WON!!!!\n")
-    else:
-        print(f"\nYou loseeee\nThe hand was {hand}")
+    if user_hand == computer_hand:
+        print("\nIt's a TIE\n")
+
+    elif user_hand == 'r':
+        if computer_hand == 'p':
+            print(f"You lost the computer's hand was {computer_hand}\n")
+        elif computer_hand == 's':
+            print(f"You Won the computer's hand was {computer_hand}\n")
+
+    elif user_hand == 'p':
+        if computer_hand == 's':
+            print(f"You lost the computer's hand was {computer_hand}\n")
+        elif computer_hand == 'r':
+            print(f"You Won the computer's hand was {computer_hand}\n")
+
+    elif user_hand == 's':
+        if computer_hand == 'r':
+            print(f"You lost the computer's hand was {computer_hand}\n")
+        elif computer_hand == 'p':
+            print(f"You Won the computer's hand was {computer_hand}\n")
 
 
 while True:
